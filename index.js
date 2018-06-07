d846dea671c058e2d13d54a751e0a22f53f95dec
blevm <mjblevins7@gmail.com>
2018-06-07 09:59:08 -0400

    table solution

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+const interval = document.getElementById('interval');
+const foreground = document.getElementById('foreground');
+
+document.getElementById('save_lincoln').addEventListener("click", (event) => {
+  saveLincoln(parseInt(interval.value));
+});
+
+function saveLincoln(ourinterval) {
+  foreground.style.opacity = 1;
+
+  if (isNaN(ourinterval)) {
+    ourinterval = 1000;
   }
-  $('#foreground').fadeOut(interval);
+
+  setInterval(function () {
+    foreground.style.opacity -= 0.01;
+  }, (ourinterval/50));
 }
